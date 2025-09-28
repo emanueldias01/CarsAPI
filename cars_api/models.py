0e9fe6d7ad425a5de0f4eeedd3f2e38981b3688b
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2025-09-28 16:37:53 -0300

    car model

---
+++ cars_api/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Text
+
+from database import Base
+
+class Car(Base):
+    __tablename_ = 'cars'
+
+    id = Column('id', Integer, primary_key=True, index=True, autoincrement=True)
+    brand = Column('brand', String, nullable=False)
+    model = Column('model', String, nullable=False)
+    color = Column('color', String, nullable=True)
+    factory_year = Column('factory_year', String, nullable=False)
+    model_year = Column('model_year', String, nullable=True)
+    description = Column('description', Text, nullable=True)
