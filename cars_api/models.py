531f6968aee9a7f0f6522421059bb40cf966eb76
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2025-09-28 16:53:08 -0300

    create migrations

--- cars_api/models.py
+++ cars_api/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String, Text
 from database import Base
 
 class Car(Base):
-    __tablename_ = 'cars'
+    __tablename__ = 'cars'
 
     id = Column('id', Integer, primary_key=True, index=True, autoincrement=True)
     brand = Column('brand', String, nullable=False)
